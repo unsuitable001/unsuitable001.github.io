2a54cc7ae2cf4b6441c100c79874797cf029837e
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-05-10 21:26:58 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-function toggleMenu() {
-    $('.ui.sidebar').sidebar('toggle');
+var scrH = 0;
+var scrW = 0;
+
+document.body.onload = function (){
+    scrH = document.body.clientHeight;
+    scrW = document.body.clientWidth;
+    let x = document.getElementById('intro').style;
+    x.height = scrH - document.getElementById('menu').offsetHeight + 'px';
 }
 
-function fixMBMT() {    //a dirty fix for a finicky glitch
-    toggleMenu();
-    setTimeout(function(){ toggleMenu(); }, 10);
+document.body.onwheel = function () {
+    console.log("hhgsfdhg");
 }
 
+function openMenu(){
+    console.log("nice");
+    document.getElementById('maindiv').classList.add("rotate");
+}

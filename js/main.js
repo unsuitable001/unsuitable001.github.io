0b61e035e85e769c2f9f1bae5949240bdf491141
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-05-11 13:24:58 +0530

    Adding main menu style
    main menu style added. entry is still empty. semantic css [basic] added.
    
    +1 step towards issue #2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 var scrH = 0;
 var scrW = 0;
-
+var maindiv;
+var hiddenmenu;
 document.body.onload = function (){
     scrH = document.body.clientHeight;
     scrW = document.body.clientWidth;
     let x = document.getElementById('intro').style;
     x.height = scrH - document.getElementById('menu').offsetHeight + 'px';
+    maindiv = document.getElementById('maindiv');
+    hiddenmenu = document.getElementById('hiddenmenu');
+    // hiddenmenu = document.createElement('div')
+    // hiddenmenu.innerHTML = "<div class='ui segment' id='hiddenmenu' style='margin: 0; padding: 0; height: "
+    //     + scrW + "px; width: "+ scrH + "px; top: -" + scrW + "px; position: absolute !important;'>"
+    //     + "<div><a> hdfhfdddfhdahsdghd</a></div>"
+    //     +"</div>";
+    // hiddenmenu = document.createElement('div');
+    // hiddenmenu.style = "'height: "+ scrW + "px; width: "+ scrH + "px; top: -" + scrW + "px; position: absolute !important;'>";
+    // hiddenmenu.class = 'ui red inverted segment';
+    // hiddenmenu.innerHTML = 'dydyfysdfuysdfgyufdyugfsdyu';
+    console.log(hiddenmenu);
+    // document.body.insertBefore(hiddenmenu ,maindiv);
+    // hiddenmenu.style.top = '-' + scrH + 'px !important';
+    // hiddenmenu.style.width = scrH + 'px';
+    // hiddenmenu.style.height = scrW + 'px';
 }
 
 document.body.onwheel = function () {
@@ -14,5 +31,7 @@ document.body.onwheel = function () {
 
 function openMenu(){
     console.log("nice");
-    document.getElementById('maindiv').classList.add("rotate");
+    maindiv.classList.add("rotate");
+    hiddenmenu.classList.add("mReveal");
+    document.body.classList.add("noscroll");
 }

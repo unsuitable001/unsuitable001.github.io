c4182d2e6cd828149fc2577b216e0ce087f6f1a1
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-05-12 18:54:45 +0530

    Add files via upload
    +1 step towards #2

--- js/home.js
+++ js/home.js
@@ -1,6 +1,26 @@
-$(document).ready(function() {
-    // if($(window).width() > 767){
-    $("#intro").height($(window).outerHeight() - $("#menu").outerHeight(true));
-    // }
-});
+var itext;
+var textArray = ['Life',
+'Javascript Development',
+'Python Development'
+];
+document.body.onload = function(){
+    console.log("ready");
+    itext = document.getElementById('itext');
+    scribbleanim();
+}
 
+document.body.onwheel = function () {
+    console.log("hhgsfdhg");
+}
+
+
+function scribbleanim(){
+    console.log("ready");
+    let tLen = itext.innerText.length - 1;
+    console.log(tLen);
+    // while (tLen != 0){
+    //     console.log(tLen);
+    //     itext.innerText[tLen] = "";
+    //     tlen = tLen - 1;
+    // }
+}
--- js/main.js
+++ js/main.js
@@ -9,29 +9,20 @@ document.body.onload = function (){
     x.height = scrH - document.getElementById('menu').offsetHeight + 'px';
     maindiv = document.getElementById('maindiv');
     hiddenmenu = document.getElementById('hiddenmenu');
-    // hiddenmenu = document.createElement('div')
-    // hiddenmenu.innerHTML = "<div class='ui segment' id='hiddenmenu' style='margin: 0; padding: 0; height: "
-    //     + scrW + "px; width: "+ scrH + "px; top: -" + scrW + "px; position: absolute !important;'>"
-    //     + "<div><a> hdfhfdddfhdahsdghd</a></div>"
-    //     +"</div>";
-    // hiddenmenu = document.createElement('div');
-    // hiddenmenu.style = "'height: "+ scrW + "px; width: "+ scrH + "px; top: -" + scrW + "px; position: absolute !important;'>";
-    // hiddenmenu.class = 'ui red inverted segment';
-    // hiddenmenu.innerHTML = 'dydyfysdfuysdfgyufdyugfsdyu';
-    console.log(hiddenmenu);
-    // document.body.insertBefore(hiddenmenu ,maindiv);
-    // hiddenmenu.style.top = '-' + scrH + 'px !important';
-    // hiddenmenu.style.width = scrH + 'px';
-    // hiddenmenu.style.height = scrW + 'px';
-}
-
-document.body.onwheel = function () {
-    console.log("hhgsfdhg");
 }
 
 function openMenu(){
-    console.log("nice");
     maindiv.classList.add("rotate");
     hiddenmenu.classList.add("mReveal");
     document.body.classList.add("noscroll");
+    maindiv.classList.remove("unrotate");
+    hiddenmenu.classList.remove("mHide");
+}
+
+function closeMenu(){
+    maindiv.classList.add("unrotate");
+    hiddenmenu.classList.add("mHide");
+    document.body.classList.remove("noscroll");
+    maindiv.classList.remove("rotate");
+    hiddenmenu.classList.remove("mReveal");
 }

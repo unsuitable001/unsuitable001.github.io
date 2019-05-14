3fc024d2c1428020d99f766db0e17e775f374b13
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-05-14 16:23:51 +0530

    V0.1 Template (#8)
    It's near-final template
    
    Fixes #2

--- js/home.js
+++ js/home.js
@@ -1,6 +1,49 @@
-$(document).ready(function() {
-    // if($(window).width() > 767){
-    $("#intro").height($(window).outerHeight() - $("#menu").outerHeight(true));
-    // }
-});
+var itext;
+var tLen = 0;
+var textArray = ['Life',
+'Javascript Development',
+'Python Development',
+'C Development',
+'Making random stuffs',
+'Being a student'
+];
+var animrandtime = 30;
+var doanim = true;
+var _interval;
+var Arrayindex = 0;
+var arraylen = textArray.length;
+var _randlen = 12;
+var _timeout;
 
+function animResetter(){
+    clearTimeout(_timeout);
+    animrandtime = 30;
+    _interval = setInterval(scribbleanim, 50);
+    return;
+}
+
+function scribbleanim(){
+    if(animrandtime > 0){
+        if(Arrayindex === arraylen){
+            Arrayindex = 0;
+        }
+        _randlen = (textArray[Arrayindex].length / 2) + 5;
+        itext.innerText =  Math.random().toString(36).substring(2, Math.floor(Math.random() * (_randlen - 4) ) + 4) + Math.random().toString(36).substring(2, Math.floor(Math.random() * (_randlen - 4) ) + 4);
+        animrandtime-=1;
+    }
+    else{
+        clearInterval(_interval);
+        itext.innerText = textArray[Arrayindex];
+        Arrayindex+=1;
+        _timeout = setTimeout(animResetter, 3000);
+        return;
+    }
+}
+
+function mainentry(){
+    let x = document.getElementById('intro').style;
+    x.height = scrH - document.getElementById('menu').offsetHeight + 'px';
+    itext = document.getElementById('itext');
+    _interval = setInterval(scribbleanim, 50);
+    return;
+}

ff579cf50f457bfa33869dab9813d1bc5c2f1da3
Soumyadip Mondal <mandal.soumyadip9714@gmail.com>
2019-10-09 13:17:56 +0530

    http to https of Q-API

--- js/home.js
+++ js/home.js
@@ -1,52 +1,3 @@
-// var itext;
-// var tLen = 0;
-// var textArray = ['Life',
-// 'Javascript Development',
-// 'Python Development',
-// 'C Development',
-// 'Making random stuffs',
-// 'Being a student'
-// ];
-// var animrandtime = 30;
-// var doanim = true;
-// var _interval;
-// var Arrayindex = 0;
-// var arraylen = textArray.length;
-// var _randlen = 12;
-// var _timeout;
-// var _isMenuOpen = false;
-
-// function homeAnimToggle(){
-//     _isMenuOpen = !_isMenuOpen;
-// }
-
-// function animResetter(){
-//     clearTimeout(_timeout);
-//     animrandtime = 30;
-//     _interval = setInterval(scribbleanim, 50);
-//     return;
-// }
-
-// function scribbleanim(){
-//     if(_isMenuOpen){
-//         return 1;
-//     }
-//     if(animrandtime > 0){
-//         if(Arrayindex === arraylen){
-//             Arrayindex = 0;
-//         }
-//         _randlen = (textArray[Arrayindex].length / 2) + 5;
-//         itext.innerText =  Math.random().toString(36).substring(2, Math.floor(Math.random() * (_randlen - 4) ) + 4) + Math.random().toString(36).substring(2, Math.floor(Math.random() * (_randlen - 4) ) + 4);
-//         animrandtime-=1;
-//     }
-//     else{
-//         clearInterval(_interval);
-//         itext.innerText = textArray[Arrayindex];
-//         Arrayindex+=1;
-//         _timeout = setTimeout(animResetter, 3000);
-//         return;
-//     }
-// }
 function Get(yourUrl){
     var Httpreq = new XMLHttpRequest(); // a new request
     Httpreq.open("GET",yourUrl,false);
@@ -59,11 +10,10 @@ function mainentry(){
     x.height = scrH - document.getElementById('menu').offsetHeight + 'px';
 
     itext = document.getElementById('itext');
-    // _interval = setInterval(scribbleanim, 50);
     let d = Math.random()*10;
     if(d<6)
     {
-        var json_obj = JSON.parse(Get("http://quotes.rest/qod.json"));
+        var json_obj = JSON.parse(Get("https://quotes.rest/qod.json"));
         itext.innerHTML = `"`+json_obj.contents.quotes[0].quote+`" -`+json_obj.contents.quotes[0].author+`<br>&copy;theysaidso.com`;
         console.log("this is the author name: "+json_obj.contents.quotes[0].quote);
         return;
